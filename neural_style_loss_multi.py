c053a507db0403a2d25592659c0d339b8df19f77
yuyakanemoto <yuyak1115@gmail.com>
2017-03-17 14:40:41 +0900

    Update neural_style_loss_multi.py

--- neural_style_loss_multi.py
+++ neural_style_loss_multi.py
@@ -2,6 +2,7 @@ import os
 import scipy.misc
 import pandas as pd
 from argparse import ArgumentParser
+import time
 
 from neural_style_loss import styleloss, imread
 
@@ -10,6 +11,9 @@ OUTPUT = 'output.csv'
 LAYER_WEIGHT_EXP = 1
 VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
 POOLING = 'max'
+NORMALIZE = 1
+VERBOSE = 1
+TIMEIT = 1
 
 def build_parser():
     parser = ArgumentParser()
@@ -28,10 +32,19 @@ def build_parser():
     parser.add_argument('--pooling',
             dest='pooling', help='pooling layer configuration: max or avg (default %(default)s)',
             metavar='POOLING', default=POOLING)
+    parser.add_argument('--normalize',
+            dest='normalize', help='normalize output values (default %(default)s)',
+            metavar='NORMALIZE', default=NORMALIZE)
+    parser.add_argument('--verbose',
+            dest='verbose', help='print raw style loss value in each iteration (default %(default)s)',
+            metavar='VERBOSE', default=VERBOSE)
+    parser.add_argument('--timeit',
+            dest='timeit', help='calculate and print the calculation time (default %(default)s)',
+            metavar='TIMEIT', default=TIMEIT)
     return parser
 
 def main():
-
+    start_time = time.time()
     parser = build_parser()
     options = parser.parse_args()
 
@@ -63,18 +76,24 @@ def main():
                     pooling=options.pooling
                     )
                 df.iloc[i,j] = style_loss
-                print('style_loss between '+str(impath1)+' and '+str(impath2)+': '+str(style_loss))
+                
+                if options.verbose == 1:
+                    print('style_loss between '+str(impath1)+' and '+str(impath2)+': '+str(style_loss))
 
             elif i>j:
                 df.iloc[i,j] = df.iloc[j,i]
             else:
                 df.iloc[i,j] = 0
     # normalize data array
-    maxval = df.values.max()
-    df = df/maxval
+    if options.normalize == 1:
+        maxval = df.values.max()
+        df = df/maxval
 
     output = options.output
     df.to_csv(output)
+    
+    if options.timeit == 1:
+        print("calculation time: %s seconds" % (time.time() - start_time))
 
 if __name__ == '__main__':
     main()
